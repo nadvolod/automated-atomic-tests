088c2178084143400bbd432552cc8739eb6eb3f3
nadvolod <nadvolod@gmail.com>
2023-06-13 14:24:33 -0400

    working createUser test

--- atomic-tests/src/test/java/com/atomic/ContactListAppSolution.java
+++ atomic-tests/src/test/java/com/atomic/ContactListAppSolution.java
@@ -1,88 +1,102 @@
 package com.atomic;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class ContactListAppSolution {
+
+    // Our web app https://thinking-tester-contact-list.herokuapp.com
     private WebDriver driver;
 
-    @BeforeEach
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        // mandatory to work with Java 11 +
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
+//    @BeforeEach
+//    public void setUp() {
+//        ChromeOptions options = new ChromeOptions();
+//        // mandatory to work with Java 11 +
+//        options.addArguments("--remote-allow-origins=*");
+//        driver = new ChromeDriver(options);
+//    }
+
+
+    @Test
+    void userCanRegister() {
+
     }
 
     @Test
-    public void userCanLogin() {
+    public void sanityTest() {
         // 1. Create a user
+        String uniqueEmail = UUID.randomUUID() + "@email.com";
+        // Define the base URL
+        RestAssured.baseURI = "https://thinking-tester-contact-list.herokuapp.com";
 
+        // Define the request body as a HashMap
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("firstName", "Nikolay");
+        requestBody.put("lastName", "Test");
+        requestBody.put("email", uniqueEmail);
+        requestBody.put("password", "Hello123");
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/users")
+                .then()
+                .extract()
+                .response();
+        System.out.println("Response:\n" + response.asString());
     }
-
     @Test
-    public void checkoutShouldWork() {
-        String username = "standard_user";
-        List<String> products = new ArrayList<>();
-        products.add("0");
-        String desiredFinalPath = "https://www.saucedemo.com/checkout-step-two.html";
-        String userCookies = "document.cookie='session-username=" + username + "';";
-        String productStorage = !products.isEmpty() ? "localStorage.setItem('cart-contents', '[" + String.join(",", products) + "]');" : "";
+    public void registerUser() {
+        // 1. Create a user
+        String uniqueEmail = UUID.randomUUID() + "@email.com";
+        // Define the base URL
+        RestAssured.baseURI = "https://thinking-tester-contact-list.herokuapp.com";
 
-        // Go to the domain
-        driver.get("https://www.saucedemo.com/");
-        // Clear the cookies and storage
-        driver.manage().deleteAllCookies();
-        ((JavascriptExecutor) driver).executeScript("localStorage.clear();");
-        // Set the new cookies and storage
-        ((JavascriptExecutor) driver).executeScript(userCookies + " " + productStorage);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        // Now go to the page
-        driver.get(desiredFinalPath);
-        // throws a nice exception
-        new WebDriverWait(driver, Duration.ofSeconds(6)).until(d -> d.findElement(By.id("checkout_summary_container")));
-        Assertions.assertEquals(1, getItemCount(), "Should have 1 item in the cart");
+        // Define the request body as a HashMap
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("firstName", "Nikolay");
+        requestBody.put("lastName", "Test");
+        requestBody.put("email", uniqueEmail);
+        requestBody.put("password", "Hello123");
 
-        /*
-         * The actual test
-         * */
-        WebElement finishButton = new WebDriverWait(driver, Duration.ofSeconds(6)).until(d -> d.findElement(By.cssSelector("[data-test='finish']")));
-        finishButton.click();
-
-        assertDoesNotThrow(this::isCheckoutComplete);
-    }
-    public int getItemCount() {
-        WebElement itemCounter;
-        try{
-            itemCounter = driver.findElement(By.cssSelector(".shopping_cart_badge"));
-            return Integer.parseInt(itemCounter.getText());
-        }
-        catch (NoSuchElementException e)
-        {
-            return 0;
-        }
-    }
-    public void isCheckoutComplete() {
-        new WebDriverWait(driver, Duration.ofSeconds(6)).until(d -> d.findElement(By.id("checkout_complete_container")));
-    }
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/users")
+                .then()
+                .assertThat()
+                .statusCode(201) // 201 is the HTTP status code for "Created"
+                .body("user.firstName", equalTo("Nikolay")) // Assert that the firstName in the response is "Nikolay"
+                .body("user.lastName", equalTo("Test")) // Assert that the lastName in the response is "Test"
+                .body("user.email", equalTo(uniqueEmail)) // Assert that the email in the response is the uniqueEmail
+                .extract()
+                .response();
+////                .body("lastName", equalTo("Test"))
+//                .body("email", equalTo(uniqueEmail))
+//                .extract()
+//                .response();
     }
 }
